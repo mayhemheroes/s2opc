8c831bba6956ef4db9de6121810f86f46a2ca36b
Azizjon Rizayev <rizaziz2002@forallsecure.com>
2026-05-06 23:06:02 +0000

    Fix mbedtls build: disable testing and add missing -Wno flags

--- mayhem/build.sh
+++ mayhem/build.sh
@@ -37,7 +37,9 @@ export CXXFLAGS="${CXXFLAGS:-} -Wno-unused-but-set-variable -Wno-unterminated-st
 cmake -DPYTHON_EXECUTABLE="/usr/bin/python3" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-      -DCMAKE_C_FLAGS="-Wno-unused-but-set-variable" \
+      -DCMAKE_C_FLAGS="-Wno-unused-but-set-variable -Wno-unterminated-string-initialization" \
+      -DENABLE_TESTING=OFF \
+      -DENABLE_PROGRAMS=OFF \
       $WORK/mbedtls-2.*
 make -j$(nproc)
 make -j$(nproc) install
